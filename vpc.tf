7ca5a9ca2d8ca9786fa90a5d4d7c5e9f2d5ef872
KazikKluz <kazik.kluz@gmail.com>
2024-10-13 13:29:06 +0100

    basic subnet configuration

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,44 @@
-resource "aws_vpc" "rs_vpc" {
+resource "aws_vpc" "RS_VPC" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
     Name = "RS VPC"
+  }
+}
+
+resource "aws_subnet" "Public_A" {
+  vpc_id     = aws_vpc.RS_VPC.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "RS Public A"
+  }
+}
+
+resource "aws_subnet" "Public_B" {
+  vpc_id     = aws_vpc.RS_VPC.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "RS Public B"
+  }
+}
+
+resource "aws_subnet" "Private_A" {
+  vpc_id     = aws_vpc.RS_VPC.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "RS Private A"
+  }
+}
+
+resource "aws_subnet" "Private_B" {
+  vpc_id     = aws_vpc.RS_VPC.id
+  cidr_block = "10.0.4.0/24"
+
+  tags = {
+    Name = "RS Private B"
   }
 }
