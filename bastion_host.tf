001f22942edff57869ccbe89952adca5c92f9a8f
KazikKluz <kazik.kluz@gmail.com>
2024-10-13 18:30:26 +0100

    updated data resource for AMI

--- bastion_host.tf
+++ bastion_host.tf
@@ -1,10 +1,10 @@
 # Get latest AMI ID for Amazon Linux2 OS
-data "aws_ami" "amzlinux2" {
+data "aws_ami" "al2023" {
   most_recent = true
   owners      = ["amazon"]
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-gp2"]
+    values = ["al2023-ami-2023.*-x86_64"]
   }
   filter {
     name   = "root-device-type"
@@ -21,13 +21,15 @@ data "aws_ami" "amzlinux2" {
 }
 
 resource "aws_instance" "bastion" {
-  ami                         = data.aws_ami.amzlinux2.id
-  instance_type               = "t3.micro"
+  ami                         = data.aws_ami.al2023.id
+  instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public_a.id
   associate_public_ip_address = true
 
+  
+
   security_groups = [
-    aws_security_group.allow_http_and_ssh.name
+    aws_security_group.allow_http_and_ssh.id
   ]
 
   tags = {
